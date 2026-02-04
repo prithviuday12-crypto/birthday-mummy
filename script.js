0084759fb24dd216140fe6fc535f96fce0154430
prithviuday12-crypto <prithviuday12@gmail.com>
2026-02-04 22:43:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [
   "1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg",
-  "8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg"
+  "8.jpg","9.jpg","10.jpg","11.jpg","12.jpg","13.jpg","14.jpg"
 ];
 
 let current = 0;
@@ -21,4 +21,5 @@ document.body.addEventListener("click", () => {
   const music = document.getElementById("bgMusic");
   music.play();
 });
+
 
